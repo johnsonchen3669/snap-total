bd4f7877d7b11eee73680ba5a44aa3ebf0ffe69c
johnsonchen3669 <johnsonchen3669@gmail.com>
2026-04-17 23:14:17 +0800

    update: 使用 cross-env 設定預部署環境變數，並更新 package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/snap-total/',
+  base: process.env.DEPLOY_TARGET === 'ghpages' ? '/snap-total/' : '/',
   plugins: [react()],
 })
